00b06eecefefccdfefcbc03ec2a0593a2dd92180
Anitha <ntanithasaravanan@gmail.com>
2025-09-13 10:51:38 +0530

    Setup trainee-tracker for Vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Vercel expects 'dist' by default
-  },
-  server: {
-    port: 5173,
-  },
+    outDir: 'dist'  // Vercel expects the build folder to be 'dist'
+  }
 });
